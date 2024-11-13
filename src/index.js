b73697bf4f9e60bfb298a5ff02802ebbd97e6fb6
george <georgewk1809@gmail.com>
2024-11-13 12:37:25 +0000

    add update and delete

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.get('/api/products', async (req, res) => {
 
 app.get('/api/products/:id', async (req, res) => {
   try {
-    const {id} = req.params
+    const { id } = req.params
 
     const product = await Product.findById(id)
     res.status(200).json(product)
@@ -41,6 +41,45 @@ app.post('/api/products', async (req, res) => {
   try {
     const product = await Product.create(req.body)
     res.status(201).json(product)
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    })
+  }
+})
+
+app.patch('/api/products/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const product = await Product.findByIdAndUpdate(id, req.body)
+    if (!product) {
+      return res.status(404).json({
+        message: "product not found"
+      })
+    }
+    const updatedProduct = await Product.findById(id)
+    res.status(201).json(updatedProduct)
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    })
+  }
+})
+
+app.patch('/api/product/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const product = await Product.findByIdAndDelete(id)
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found"
+      })
+    }
+    res.status(200).json({
+      message: "Product deleted successfully"
+    })
   } catch (error) {
     res.status(500).json({
       message: error.message
