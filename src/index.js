be068144f167477593bd4fca526cd6d5f06a5107
george <georgewk1809@gmail.com>
2024-11-13 01:15:23 +0000

    add create and read

--- src/index.js
+++ src/index.js
@@ -2,9 +2,51 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/product.model')
 
 const app = express()
 const port = process.env.PORT || 8888
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send("hello")
+})
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find({})
+    res.status(200).json(products)
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    })
+  }
+})
+
+app.get('/api/products/:id', async (req, res) => {
+  try {
+    const {id} = req.params
+
+    const product = await Product.findById(id)
+    res.status(200).json(product)
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    })
+  }
+})
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(201).json(product)
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    })
+  }
+})
 
 mongoose.connect(process.env.CONN_STRING)
 .then(() => {
@@ -15,8 +57,4 @@ mongoose.connect(process.env.CONN_STRING)
 })
 .catch((err) => {
   console.log(err)
-})
-
-app.get('/', (req, res) => {
-  res.send("hello")
 })
