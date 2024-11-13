a644c96412a8faf8d6358bafb364c3cab29a449b
george <georgewk1809@gmail.com>
2024-11-13 18:42:25 +0000

    remove unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/product.model')
 const ProductRoutes = require('./routes/product.route')
 
 const app = express()
