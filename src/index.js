db459ebe5e66bc3083921601871762fe3045c0ae
george <georgewk1809@gmail.com>
2024-11-13 00:39:47 +0000

    create connection to mongodb

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 require('dotenv').config()
 
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 
+const app = express()
 const port = process.env.PORT || 8888
+
+mongoose.connect(process.env.CONN_STRING)
+.then(() => {
+  console.log('connected to database')
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`)
+  })
+})
+.catch((err) => {
+  console.log(err)
+})
 
 app.get('/', (req, res) => {
   res.send("hello")
-})
-
-app.listen(port, () => {
-  console.log(`server running on port ${port}`)
 })
